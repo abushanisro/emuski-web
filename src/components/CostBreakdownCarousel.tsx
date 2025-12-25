ce1f79d120958c63f32d1f5caea04c76bce78cc6
abushanisro <khanabushan09876@gmail.com>
2025-12-26 02:46:58 +0530

    all layout back

--- src/components/CostBreakdownCarousel.tsx
+++ src/components/CostBreakdownCarousel.tsx
@@ -9,7 +9,7 @@ interface CostBreakdownCarouselProps {
   className?: string
 }
 
-const PAGES = [1, 3, 4, 5, 6, 7, 8]
+const PAGES = [1, 3, 4, 5, 7, 8]
 
 export function CostBreakdownCarousel({ className = '' }: CostBreakdownCarouselProps) {
   const [api, setApi] = useState<CarouselApi>()
